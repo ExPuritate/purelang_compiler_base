361ac408f0c454b26a73de0f6292fd6e913e78e8
daiyan1145 <qazwsx_dy@163.com>
2025-07-20 18:11:38 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub extern crate global;
+pub extern crate memchr;
+pub extern crate unicode_properties;
+pub extern crate unicode_xid;
+
+pub use global::Result as AnyResult;
+pub use rustc_literal_escaper as lit_escaper;
